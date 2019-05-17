2676b5dfa045644ed2141f4eb3e8b9feb6b84516
hugodessomme <hdessomme@gmail.com>
2019-05-17 14:14:24 +0200

    Add BurgerBuilder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = props => (
   <Auxiliary>
-    <div />
-    <main>{props.children}</main>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 );
 
