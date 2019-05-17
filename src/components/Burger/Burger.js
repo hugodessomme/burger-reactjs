f04d5f50025038bbd17744c5b7162171af6c9b79
hugodessomme <hdessomme@gmail.com>
2019-05-17 14:55:19 +0200

    List burger ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = props => {
+  const transformIngredients = Object.keys(props.ingredients).map(
+    ingredientKey =>
+      [...Array(props.ingredients[ingredientKey])].map((_, index) => (
+        <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+      ))
+  );
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+burger.propTypes = {
+  ingredients: PropTypes.object.isRequired,
+};
+
+export default burger;
