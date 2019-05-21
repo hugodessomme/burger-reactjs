931e01464863d5d7d83d3933c371b0a091082be8
hugodessomme <hdessomme@gmail.com>
2019-05-21 13:37:12 +0200

    Add Orders container

---
+++ src/components/Order/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Order.module.css';
+
+const Order = () => (
+  <div className={classes.Order}>
+    <p>Ingredients:</p>
+    <p>
+      Price: <strong />
+    </p>
+  </div>
+);
+
+export default Order;
